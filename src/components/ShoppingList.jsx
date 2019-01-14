c72bd2733722d1d0bd9ba6f7ab53f65a8fd8798e
Stommy123 <Stommy49@Yahoo.com>
2019-01-13 21:04:27 -0500

    added item headers

--- src/components/ShoppingList.jsx
+++ src/components/ShoppingList.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
+import ItemHeader from './ItemHeader.jsx'
 import Item from './Item.jsx'
 
 const ShoppingList = ({ inCart, needToBuy, handleChange }) =>
         <div className="shopping-list">
           <div className="to-buy">
+            <ItemHeader title="Need To Buy" />
             { needToBuy.map((item,i) => <Item symbol="+" status="add" item={item} key={i} handleChange={handleChange} /> )}
           </div>
-           <div className="cart">
-               { inCart.map((item, i) => <Item symbol="-" status="remove" item={item} key={i} handleChange={handleChange} /> )}
-           </div>
+          <div className="cart">
+            <ItemHeader title="In Cart" />
+            { inCart.map((item, i) => <Item symbol="-" status="remove" item={item} key={i} handleChange={handleChange} /> )}
+          </div>
         </div>
 
 export default ShoppingList
